f6ae0ce480d4956d92d31da18f0eb846c708b662
Victor <victorrodrigues288@gmail.com>
2024-05-13 14:59:34 -0300

    Vetores

---
+++ vetor-dias-semana/README.md
@@ -0,0 +1,7 @@
+ATIVIDADE - DESAFIO
+
+Criar um vetor que receba os dias da semana e escreva todos os dias da semana
+
+--------------------------------------------------------------------------------------
+
+CODIGO NO PORTUGOL:
---
+++ vetor-dias-semana/diassemana.js
@@ -0,0 +1,12 @@
+function executar(){
+    var semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
+
+    for (i = 0; i < semana.length; i++) {
+        console.log(semana[i]);
+        document.getElementById("resultado").innerHTML = semana[i];
+    }
+    
+
+}
+
+
