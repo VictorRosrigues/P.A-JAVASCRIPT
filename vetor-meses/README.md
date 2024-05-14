36f16570f40d23defa514010d721b18bebe56eaf
Victor <victorrodrigues288@gmail.com>
2024-05-13 22:17:47 -0300

    vetor3-meses

---
+++ vetor-meses/README.md
@@ -0,0 +1,7 @@
+ATIVIDADE - DESAFIO
+
+Criar um vetor que mostre os 12 meses do calendário.
+
+--------------------------------------------------------------------------------------
+
+CODIGO NO PORTUGOL:
---
+++ vetor-meses/meses.js
@@ -0,0 +1,6 @@
+function executar(){
+
+    var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Maio", "Junho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+    document.getElementById("resultado").innerText = meses
+}
